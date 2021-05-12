85eaaa254cfba2a8175acff40d239b34c073bd9b
Jay Herron <jay@passivelogic.com>
2021-05-12 15:17:07 -0600

    Loosens Graphiti dependency

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/GraphQLSwift/GraphQL.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/GraphQLSwift/Graphiti.git", .upToNextMinor(from: "0.22.0")),
+        .package(url: "https://github.com/GraphQLSwift/Graphiti.git", .upToNextMajor(from: "0.22.0")),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.1.0"))
     ],
     targets: [
