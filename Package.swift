800da39642feb942eec4ae7d68e51ccb9d2c0769
Jay Herron <NeedleInAJayStack@protonmail.com>
2021-10-27 18:14:42 -0600

    Bumps Graphiti to next major version

--- Package.swift
+++ Package.swift
@@ -7,10 +7,9 @@ let package = Package(
         .library(name: "GraphQLRxSwift", targets: ["GraphQLRxSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", .upToNextMajor(from: "2.0.0")),
-        // TODO: Mainline after this PR is merged: https://github.com/GraphQLSwift/Graphiti/pull/69
-        .package(url: "https://github.com/NeedleInAJayStack/Graphiti.git", .branch("fix/ScalarInput")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.1.0"))
+        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "2.0.0"),
+        .package(url: "https://github.com/GraphQLSwift/Graphiti.git", from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.1.0")
     ],
     targets: [
         .target(name: "GraphQLRxSwift", dependencies: ["GraphQL", "Graphiti", "RxSwift"]),
